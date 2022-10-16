bbb8a92c2a6e677a9690b612004466f60074453f
nasirdiu <nasir.joypurhat@gmail.com>
2022-10-16 10:45:40 +0600

    excrpt error update versition

--- inc/default.php
+++ inc/default.php
@@ -11,3 +11,20 @@ add_theme_support('title-tag');
 add_theme_support( 'post-thumbnails', array('page','post') );
 
 add_image_size('post-thumbnails', 970, 350, true);
+
+
+// Except to 40 Word versition update er jono error
+
+/*
+function nasir_excerpt_more($more){
+    return '<a href="'.get_permalink( $post->ID) . '">' . 'Read More' . '</a>';
+
+}
+add_filter('excerpt_more','nasir_excerpt_more');
+
+function nasir_excerpt_lenght($length){
+     return 40;
+  }
+  add_filter('excerpt_length', 'nasir_excerpt_lenght', 999);
+
+*/
