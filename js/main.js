2cdbe99eaa89020d88c95119e86d0a7642b50855
nasirdiu <nasir.joypurhat@gmail.com>
2022-10-22 18:26:40 +0600

    owl-slider

--- js/main.js
+++ js/main.js
@@ -4,4 +4,8 @@ jQuery(document).ready(function () {
           mode: 'fade',
           captions: true,
         });
+        jQuery(".owl-carousel").owlCarousel({
+          autoplay: true,
+          items:1,
+        });
       });
