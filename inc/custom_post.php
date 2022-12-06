f069757c7d20165ca8c9246b34b2dacd835eac2e
nasirdiu <nasir.joypurhat@gmail.com>
2022-12-06 10:27:42 +0600

    custome-post

--- inc/custom_post.php
+++ inc/custom_post.php
@@ -67,6 +67,41 @@ function custom_service(){
 }
 add_action('init', 'custom_service');
 
+function custom_project(){
+    register_post_type('project',
+    array(
+        'labels'=>array(
+        'name'=>('projects'),
+        'singular_name' => ('project'),
+        'add_new' => ('Add New project'),
+        'add_new_item' => ('Add New project'),
+        'edit_item' => ('Edit project'),
+        'new_item' => ('New project'),
+        'view_item' => ('View project'),
+        'not_found' => ('Sorry, we cound\'n find the project you are looking for.'),
+        ),
+
+        'menu_icon' => 'dashicons-calendar',
+        'public' => true,
+        'publicly_queryable' => true,
+        'exclude_from_search' => true,
+        'menu_position' => 5, 
+        'has_archive' => true,
+        'hierarchial' => true,
+        'show_ui' => true,
+        'capability_type' => 'post',
+        'taxonomies' => array ('category'),
+        'rewrite' => array('slag' => 'project'),
+        'supports' => array('title', 'thumbnail', 'editor', 'excerpt'),
+    )
+    );
+
+    add_theme_support('post-thumbnails');
+}
+add_action('init', 'custom_project');
+
+
+
 //service category post show function 
 
 function query_post_type($query){
--- inc/default.php
+++ inc/default.php
@@ -12,6 +12,7 @@ add_theme_support( 'post-thumbnails', array('page','post','service') );
 
 add_image_size('slider', 1920, 600, true);
 add_image_size('service', 390, 250, true);
+add_image_size('project', 390, 250, true);
 add_image_size('post-thumbnails', 970, 350, true);
 
 
